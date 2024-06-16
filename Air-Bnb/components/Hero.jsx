af7d3a4a6389dab22f8eb0edaf3750e2a32cb3de
Jona Subba <sanarung.subba@gmail.com>
2024-06-16 15:53:32 +0545

    first commit

---
+++ Air-Bnb/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Hero(){
+    return(
+        <section>
+            <img src="../src/assets/Screenshot (344).png" className="" />
+            <h1 className="hero--header">Online Experiences</h1>
+            <p className="hero-text">Join unique interactive activities led by one-of-a-kind hosts-all without leaving home.</p>
+        </section>
+    )
+}
+
